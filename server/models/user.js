346c484ea8d20e46f7973dbf80c251aa674abbdb
kaushik <itiskaushikofficial@gmail.com>
2017-05-29 12:56:48 +0530

    post /todos and refactoring our code

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+var Users = mongoose.model('users',{
+    email:{
+        type:String,
+        required:true,
+        minlength:1,
+        trim:true
+    }
+});
+
+module.exports = {Users}
---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {Users} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((docs) => {
+        res.send(docs);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+
+    
+});
+
+app.listen('3000', () => {
+    console.log('Started on port 3000');
+});
