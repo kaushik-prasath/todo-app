f95351eb97fa5c195f470604c4db333649e2c006
kaushik <itiskaushikofficial@gmail.com>
2017-05-29 18:37:40 +0530

    todos/id route created

---
+++ playground/mongodb-queries.js
@@ -0,0 +1,34 @@
+
+const {ObjectID} = require('mongodb');
+
+const {Users} = require('./../server/models/user');
+const {mongoose} = require('./../server/db/mongoose');
+
+const id = '592aa468ce2b2d0e242ca028';
+
+if(!ObjectID.isValid(id)){
+    console.log('ID is not valid');
+}
+
+
+
+
+
+
+
+
+// Users.find({
+//     _id:id
+// }).then((user) => {
+//     console.log('Todos:',user);
+// });
+
+
+// Users.findById(id).then((user) => {
+//     if(!user){
+//  console.log('User not found');
+//     }
+//     console.log('Todos:',user);
+// },(err) => {
+//     console.log(err);
+// });
