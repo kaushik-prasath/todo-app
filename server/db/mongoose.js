1aa6b00eca2265c8538a85e71a912bf7e171efd0
kaushik <itiskaushikofficial@gmail.com>
2017-05-30 23:06:04 +0530

    enviromental variable added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.promise = global.promise;
 let db = {
   localhost: 'mongodb://localhost:27017/Todos',
-  mlab: 'mongodb://kaushik prasath:opsimath47@ds157621.mlab.com:57621/todoskaushik'
+  mlab: process.env.MONGODB_URI
 };
 mongoose.connect(db.mlab || db.localhost);
 
