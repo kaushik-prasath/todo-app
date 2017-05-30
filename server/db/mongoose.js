b5727db225293301d6a250629c768ff3c7fa5e2e
kaushik <itiskaushikofficial@gmail.com>
2017-05-30 12:42:54 +0530

    heroku configurations

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
-mongoose.connect('mongodb://localhost:27017/Todos');
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://kaushik prasath:opsimath47@ds149268.mlab.com:49268/todoapp'
+};
+mongoose.connect(db.mlab || db.localhost);
 
 
 module.exports = {mongoose}
+
+
+
+
+
