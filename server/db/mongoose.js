1dc70ee0d24ce352e4963f1891ee8970fb5ad657
kaushik <itiskaushikofficial@gmail.com>
2017-05-30 15:41:17 +0530

    config of online mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
 let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
+  localhost: 'mongodb://localhost:27017/Todos',
   mlab: 'mongodb://kaushik prasath:opsimath47@ds157621.mlab.com:57621/todoskaushik'
 };
 mongoose.connect(db.mlab || db.localhost);
