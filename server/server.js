2fcb28ef428f1913726cae54133013e7b162bc08
kaushik <itiskaushikofficial@gmail.com>
2017-05-31 18:18:23 +0530

    removed else if and changed to else

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 var env = process.env.NODE_ENV || 'development';
-console.log('env***',env);
+console.log(`Env=${env}`);
 
 if(env === 'development'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/Todos';  
-} else if(env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodosTest';
-    
+} else
+{
+      process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodosTest';   
 }
-
+  
 
 
 
