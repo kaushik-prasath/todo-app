8ab22af3b113658baff60ea23ba6fc3727b5e508
kaushik <itiskaushikofficial@gmail.com>
2017-06-01 11:53:51 +0530

    env vars

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
+let db = {
+    mlab: process.env.MONGODB_URI
+}
+
 mongoose.promise = global.promise;
-mongoose.connect( process.env.MONGODB_URI);
+mongoose.connect(  db.mlab || process.env.MONGODB_URI );
 
 
 module.exports = {mongoose}
