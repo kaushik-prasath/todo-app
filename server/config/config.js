8999d6d9c686d1d61089c6ad918afa138e95fff7
kaushik <itiskaushikofficial@gmail.com>
2017-06-01 11:56:54 +0530

    node env

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ console.log(`Env=${env}`);
 if(env === 'development'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/Todos';  
-} else
+} else if(env === 'test')
 {
       process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodosTest';   
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-let db = {
-    mlab: process.env.MONGODB_URI
-}
 
 mongoose.promise = global.promise;
-mongoose.connect(  db.mlab || process.env.MONGODB_URI );
+mongoose.connect(process.env.MONGODB_URI );
 
 
 module.exports = {mongoose}
